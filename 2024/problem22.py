1b5009e4cf19bc1564c7be086ee9f0b7f837163b
Ram Mehta <ram.mehta@gmail.com>
2024-12-22 15:41:29 -0500

    cleanup.

--- 2024/problem22.py
+++ 2024/problem22.py
@@ -13,18 +13,6 @@ def calc_next(s):
     return s
 
 
-def calc_next_n_simple(secret, iters=2000):
-    last4 = []
-    for _ in range(iters):
-        secret = calc_next(secret)
-
-        last4.append(secret)
-        if len(last4) > 4:
-            last4.pop(0)
-
-    return secret
-
-
 def calc_next_n(secret, iters=2000):
     yield secret
     for _ in range(iters):
@@ -42,30 +30,38 @@ def get_changes(sequence):
         yield s2 - s1
 
 
-def get_window_map(sequence, changes, windows, best_score):
-    result = {}
+def iter_index(iter, cache, index):
+    curr_index = len(cache)
+    while curr_index <= index:
+        cache.append(next(iter))
+        curr_index += 1
+    return cache[index]
+
+
+def get_window_map(sequence, windows, best_score):
+    seen = set()
+    changes = list(get_changes(sequence))
     for i in range(len(changes) - 3):
         key = tuple(changes[i : i + 4])
-        if key not in result:
+        if key not in seen:
+            seen.add(key)
             windows[key] += sequence[i + 4]
-            best_score = max(best_score, windows[key])`
+            best_score = max(best_score, windows[key])
     return best_score
 
 
-def get_window_maps(sequences, all_changes):
+def get_window_maps():
     windows = defaultdict(int)
     best_score = 0
-    for seq, changes in zip(sequences, all_changes):
-        best_score = get_window_map(seq, changes, windows, best_score)
+    for s in secret_numbers:
+        sequence = list(get_sequence(s))
+        best_score = get_window_map(sequence, windows, best_score)
     return best_score
 
 
 # Part 1
-print(sum([calc_next_n_simple(s) for s in secret_numbers]))
+print(sum(list(calc_next_n(s))[-1] for s in secret_numbers))
 
 
 # Part 2
-sequences = list(list(get_sequence(s)) for s in secret_numbers)
-all_changes = list(list(get_changes(s)) for s in sequences)
-best_score = get_window_maps(sequences, all_changes)
-print(best_score)
+print(get_window_maps())

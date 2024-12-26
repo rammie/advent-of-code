17f4197cea9ab4ac526ea0bd517516369d5c468f
Ram Mehta <ram.mehta@gmail.com>
2024-12-25 23:20:33 -0500

    Make p24 a bit less verbose.

--- 2024/problem24.py
+++ 2024/problem24.py
@@ -34,9 +34,6 @@ class Instruction:
         var2 = values.get(self.var2, 0)
         values[self.result] = ops[self.op](var1, var2)
 
-    def __iter__(self):
-        return iter((self.var1, self.var2, self.op, self.result))
-
 
 def simulate(instructions, values, num_bits):
     for _ in range(num_bits):
@@ -83,13 +80,82 @@ def find_instruction(wire, op):
     return next(instr for instr in instructions_by_input[wire] if instr.op == op)
 
 
-def swap_wires(wire1, wire2):
+def swap_wires(wire1, wire2, result):
     instr1 = instructions_by_result[wire1]
     instr2 = instructions_by_result[wire2]
     instr1.swap_result(instr2)
     instructions_by_result[wire1] = instr2
     instructions_by_result[wire2] = instr1
-    return {wire1, wire2}
+    result.update({wire1, wire2})
+
+
+def find_and_swap_var(wire, search_var, search_op, result):
+    swap = find_instruction(search_var, search_op)
+    swapped_var = swap.var2 if swap.var1 == search_var else swap.var1
+    swap_wires(wire, swapped_var, result)
+    return swapped_var
+
+
+def find_and_swap_result(wire, search_var, search_op, result):
+    swap = find_instruction(search_var, search_op)
+    swap_wires(wire, swap.result, result)
+    return swap.result
+
+
+def rewire():
+    """Work from the first input applying the logic above to make swaps."""
+    result = set()
+
+    # Logic for the first and last bits are different. They are not swapped in the input.
+    z_0 = instructions_by_result["z00"]
+    a_0 = instructions_by_inputs[("x00", "y00", "XOR")]
+    if z_0 != a_0:
+        swap_wires(z_0.result, a_0.result, result)
+
+    b_prev = instructions_by_inputs[("x00", "y00", "AND")]
+    d_prev = b_prev
+
+    for bit in range(1, num_bits - 1):
+        a_n = instructions_by_inputs[(var_name("x", bit), var_name("y", bit), "XOR")]
+        b_n = instructions_by_inputs[(var_name("x", bit), var_name("y", bit), "AND")]
+
+        c_n_desc = (a_n.result, d_prev.result, "AND")
+        if c_n_desc not in instructions_by_inputs:
+            find_and_swap_var(a_n.result, d_prev.result, "AND", result)
+            c_n_desc = (a_n.result, d_prev.result, "AND")
+
+        c_n = instructions_by_inputs[c_n_desc]
+        if c_n.result.startswith("z"):
+            find_and_swap_result(c_n.result, a_n.result, "XOR", result)
+
+        d_n_desc = (c_n.result, b_n.result, "OR")
+        if d_n_desc not in instructions_by_inputs:
+            find_and_swap_var(b_n.result, c_n.result, "OR", result)
+            d_n_desc = (c_n.result, b_n.result, "OR")
+
+        d_n = instructions_by_inputs[d_n_desc]
+
+        z_n_desc = (a_n.result, d_prev.result, "XOR")
+        if z_n_desc not in instructions_by_inputs:
+            find_and_swap_var(a_n.result, d_prev.result, "XOR", result)
+
+        z_n = instructions_by_result[var_name("z", bit)]
+        expected_z_n = instructions_by_inputs[z_n_desc]
+        if expected_z_n != z_n:
+            swap_wires(z_n.result, expected_z_n.result, result)
+
+        b_prev = b_n
+        d_prev = d_n
+
+    z_last = instructions_by_result[var_name("z", num_bits - 1)]
+    assert z_last == d_n
+    return result
+
+
+print(",".join(sorted(b for b in rewire())))
+
+
+# Addendum
 
 
 def binary_addition(x_val, y_val):
@@ -122,53 +188,5 @@ def binary_addition(x_val, y_val):
     assert x_val + y_val == z_val
 
 
-def rewire():
-    """Work from the first input applying the logic above to make swaps."""
-    result = set()
-    b_prev = instructions_by_inputs[("x00", "y00", "AND")]
-    d_prev = b_prev
-
-    for bit in range(1, num_bits - 1):
-        z_n = instructions_by_result[var_name("z", bit)]
-        a_n = instructions_by_inputs[(var_name("x", bit), var_name("y", bit), "XOR")]
-        b_n = instructions_by_inputs[(var_name("x", bit), var_name("y", bit), "AND")]
-
-        c_n_desc = (a_n.result, d_prev.result, "AND")
-        if c_n_desc not in instructions_by_inputs:
-            swap = find_instruction(d_prev.result, "AND")
-            swapped_var = swap.var2 if swap.var1 == d_prev.result else swap.var1
-            c_n_desc = (swapped_var, d_prev.result, "AND")
-            result.update(swap_wires(a_n.result, swapped_var))
-
-        c_n = instructions_by_inputs[c_n_desc]
-        if c_n.result.startswith("z"):
-            swap = find_instruction(a_n.result, "XOR")
-            result.update(swap_wires(c_n.result, swap.result))
-
-        d_n_desc = (c_n.result, b_n.result, "OR")
-        if d_n_desc not in instructions_by_inputs:
-            swap = find_instruction(c_n.result, "OR")
-            swapped_var = swap.var2 if swap.var1 == c_n.result else swap.var1
-            d_n_desc = (c_n.result, swapped_var, "OR")
-            result.update(swap_wires(b_n.result, swapped_var))
-
-        d_n = instructions_by_inputs[d_n_desc]
-
-        z_n_desc = (a_n.result, d_prev.result, "XOR")
-        if z_n_desc not in instructions_by_inputs:
-            swap = find_instruction(d_prev.result, "XOR")
-            swapped_var = swap.var2 if swap.var1 == d_prev.result else swap.var1
-            result.update(swap_wires(a_n.result, swapped_var))
-        else:
-            expected_z_n = instructions_by_inputs[z_n_desc]
-            if expected_z_n != z_n:
-                result.update(swap_wires(z_n.result, expected_z_n.result))
-
-        b_prev = b_n
-        d_prev = d_n
-    return result
-
-
 binary_addition(3, 3)
 binary_addition(5, 6)
-print(",".join(sorted(b for b in rewire())))
